ca82bcef17df1d40a101b453089fcc3a8c06e6a4
limed <limed@mozilla.com>
2016-08-10 17:43:18 -0700

    Added launch config and a atlas resource

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -34,6 +34,21 @@ resource "aws_security_group" "user-management" {
     }
 }
 
+resource "atlas_artifact" "nubis-nat" {
+  count = "${var.enabled}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  name = "nubisproject/nubis-user-management"
+  type = "amazon.image"
+
+  metadata {
+    project_version = "${var.nubis_version}"
+  }
+}
+
 resource "aws_autoscaling_group" "user-management" {
     count               = "${var.enabled}"
     name                = "user-management-${var.project} - ${aws_launch_configuration.user-management.name}"
@@ -67,5 +82,25 @@ resource "aws_autoscaling_group" "user-management" {
         value               = "${var.environments}"
         propagate_at_launch = true
     }
+
+}
+
+resource "aws_launch_configuration" "user-management" {
+    count = "${var.enabled}"
+
+    # Somewhat nasty, since Atlas doesn't have an elegant way to access the id for a region
+    # the id is "region:ami,region:ami,region:ami"
+    # so we split it all and find the index of the region
+    # add on, and pick that element
+    image_id = "${ element(split(",",replace(atlas_artifact.nubis-user-management.id,":",",")) ,1 + index(split(",",replace(atlas_artifact.nubis-user-management.id,":",",")), var.region)) }"
+
+    instance_type   = "t2.nano"
+    key_name        = "${var.key_name}"
+
+    security_groups = [
+      "${aws_security_group.user-management.id}",
+      "${var.internet_security_group_id}",
+      "${var.shared_services_security_group_id}",
+    ]
 
 }
