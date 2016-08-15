77fe6d9b2dd16d32628002752fa75d6c0e78df00
limed <limed@mozilla.com>
2016-08-15 12:33:36 -0700

    Moving this data into base

--- nubis/puppet/confd.pp
+++
@@ -1,8 +0,0 @@
-
-file { '/etc/confd/conf.d/nubis-users.toml':
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0644',
-    source => 'puppet:///nubis/files/nubis-users.toml',
-}
